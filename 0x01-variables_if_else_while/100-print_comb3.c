1093cf8e632e8bf3c6b88a8da3b9ae51bb06e208
Cron-nos <dfmicaiah@gmail.com>
2023-06-18 15:15:04 -0700

    Taking out the last ',' in Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two-digit numbers with
- * different digits. Treating combinations like 01 and 10 as the same.
+ * main - Prints all possible combinations of two-digit numbers with
+ *	different digits, treating combinations like 01 and 10 as the same.
  *
  * Return: Always 0 (Success)
  */
@@ -15,18 +15,15 @@ int main(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = num1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			if (num1 != num2)
-			{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
+			putchar(num1 + '0');
+			putchar(num2 + '0');
 
-				if (!(num1 == 9 && num2 == 8))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
