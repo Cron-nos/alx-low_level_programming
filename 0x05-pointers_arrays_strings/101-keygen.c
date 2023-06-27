b07f0fcf939693524bd51739a48f39af88547d7f
Cron-nos <dfmicaiah@gmail.com>
2023-06-26 20:47:05 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int passwd[100];
+	int a, n, sum;
+
+	sum = 0;
+	srand(time(NULL));
+	for (a = 0; a < 100; a++)
+	{
+		passwd[a] = rand() % 78;
+		sum += (passwd[a] + '0');
+		putchar(passwd[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
