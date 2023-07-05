32c42c51ba221ca39450ac3702c4016a53ed7f3b
Cron-nos <dfmicaiah@gmail.com>
2023-07-04 20:01:21 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: an array
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int p;
+	int q;
+
+	for (p = 0; p < 8; p++)
+	{
+		for (q = 0; q < 8; q++)
+			_putchar(a[p][q]);
+		_putchar('\n');
+	}
+}
